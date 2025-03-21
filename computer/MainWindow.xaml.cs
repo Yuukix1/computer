d9a62475c298e05d337ce4d9fc74797dd102fde8
BabakAdam <babaka@kkszki.hu>
2025-03-21 12:20:00 +0100

    Add project files.

---
+++ computer/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace computer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            adatbetoltes();
+        }
+        private void adatbetoltes()
+        {
+            listbox1.Items.Clear();
+
+            var osData = Connect.GetData("SELECT * FROM OSystem");
+
+            listbox1.Items.Add("Operating Systems");
+
+            foreach (DataRow row in osData.Rows)
+            {
+                listbox1.Items.Add($"{row["Id"]} - {row["Name"]}");
+            }
+
+            var compData = Connect.GetData("SELECT * FROM Comp");
+
+            listbox1.Items.Add("Computers");
+
+            foreach (DataRow row in compData.Rows)
+            {
+                listbox1.Items.Add($"{row["Id"]} - {row["Brand"]} - {row["Type"]} - {row["Display"]} inch - {row["Memory"]} GB - {row["CreatedTime"]}");
+            }
+        }
+
+
+    }
+}
---
+++ computer/connect.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+public class Connect
+{
+    private const string connectionString = "server=localhost;database=computer;user=root;password=;";
+
+    public static MySqlConnection GetConnection()
+    {
+        return new MySqlConnection(connectionString);
+    }
+
+    public static DataTable GetData(string query)
+    {
+        using (var connection = GetConnection())
+        {
+            connection.Open();
+            using (var command = new MySqlCommand(query, connection))
+            {
+                using (var adapter = new MySqlDataAdapter(command))
+                {
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    return table;
+                }
+            }
+        }
+    }
+
+    public static void Update(string query)
+    {
+        using (var connection = GetConnection())
+        {
+            connection.Open();
+            using (var command = new MySqlCommand(query, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
